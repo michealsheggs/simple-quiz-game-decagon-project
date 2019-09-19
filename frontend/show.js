8a2de2c5e21bb8a377f571a14757252eedf4d10c
Iluore Emmanuel <michealsheggs@gmail.com>
2019-09-19 17:22:31 +0100

    working on the scores wiew page

---
+++ frontend/show.js
@@ -0,0 +1,21 @@
+$.getJSON('http://localhost:3000/questions', function(quizQuestions){
+    $.each(quizQuestions, function(key, value){
+        const populateData = 
+        `
+        <table class="table table-border table-striped">
+        <thead>
+            <th>${value.id}</th><th>the correct answers</th><th>the wrong scores</th>
+        </thead>
+        <tbody>
+          <tr> 
+              <td class="bg-success">${value.answer}</td>         
+          </tr>                        
+        </tbody>
+    </table>
+        `
+        ;
+        $(".tableContent").append(populateData);
+    });
+   
+}
+);
